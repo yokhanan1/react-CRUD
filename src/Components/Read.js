16a68f21701b10a551a53f73be604433ff09b5c6
Yokhanan Olotu <yokhanan1@gmail.com>
2022-11-17 07:58:07 +0100

    add complete read component

--- src/Components/Read.js
+++ src/Components/Read.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import { Table } from 'semantic-ui-react'
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function Read() {
+    
+    const [APIData, setAPIData] = useState([]);
+    
+    useEffect(() => {
+        axios.get(`https://6373df540bb6b698b6178f9c.mockapi.io/testData`).then((response) => {setAPIData(response.data)})
+    },[])
 
     return(
         <div>
             <Table singleLine>
+
                 <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell />
-                    <Table.HeaderCell>Name</Table.HeaderCell>
-                    <Table.HeaderCell>Registration Date</Table.HeaderCell>
-                    <Table.HeaderCell>E-mail address</Table.HeaderCell>
-                    <Table.HeaderCell>Premium Plan</Table.HeaderCell>
-                </Table.Row>
+                    <Table.Row>
+                        <Table.HeaderCell>First Name</Table.HeaderCell>
+                        <Table.HeaderCell>Last Name</Table.HeaderCell>
+                        <Table.HeaderCell>Checked</Table.HeaderCell>
+                    </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                <Table.Row>
-                    <Table.Cell>John Lilki</Table.Cell>
-                    <Table.Cell>September 14, 2013</Table.Cell>
-                    <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-                    <Table.Cell>No</Table.Cell>
-                </Table.Row>
+                    {APIData.map((data) => {
+                        return (
+                            <Table.Row>
+                                <Table.Cell>{ data.firstName }</Table.Cell>
+                                <Table.Cell>{ data.lastName }</Table.Cell>
+                                <Table.Cell>{ data.checkbox ? 'Checked' : 'Unchecked' }</Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
                 </Table.Body>
+
             </Table>
         </div>
     )
